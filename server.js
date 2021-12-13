126428d50ee8ed682f69ff97ad9c7d72decbddfe
davidcwh <dcwh12321@gmail.com>
2021-12-13 19:38:06 +0800

    add index

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-type,Accept,x-access-token,X-Key"
+  );
+  if (req.method == "OPTIONS") {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
+
+require('./routes')(app);
+
+module.exports = app;
